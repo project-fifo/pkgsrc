653673a5b7b6a2dccc185862d53c1383660c327c
Jonathan Perkin <jperkin@joyent.com>
2014-04-29 15:11:21 +0100

    Move INIT_SYSTEM to bsd.prefs.mk

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -759,6 +759,14 @@ PREPEND_PATH+=		${X11BASE}/bin
 .endif
 PREPEND_PATH+=		${LOCALBASE}/bin
 
+# Support alternative init systems.
+#
+INIT_SYSTEM?=		rc.d
+.if ${INIT_SYSTEM} == "smf"
+.  include "smf.mk"
+.endif
+_BUILD_DEFS+=		INIT_SYSTEM
+
 # Wrapper framework definitions
 .include "wrapper/wrapper-defs.mk"
 
